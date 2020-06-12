ee735a85f823832e7f7cc530a976d470ab251e5d
alvinnava13 <anava8@mail.greenriver.edu>
2020-06-12 11:59:46 -0700

    Start home page and create header fragment.

---
+++ src/main/java/edu/greenriver/it/foodieblog/FoodieBlogApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.it.foodieblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/greenriver/it/foodieblog/FoodieBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.it.foodieblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
