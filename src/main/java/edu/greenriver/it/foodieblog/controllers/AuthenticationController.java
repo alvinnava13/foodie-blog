b5647079cbac0af7b9aba754a38372717bce672d
alvinnava13 <anava8@mail.greenriver.edu>
2020-06-12 17:27:08 -0700

    Created a register user form and a route for it.

---
+++ src/main/java/edu/greenriver/it/foodieblog/controllers/AuthenticationController.java
@@ -0,0 +1,25 @@
+package edu.greenriver.it.foodieblog.controllers;
+
+import edu.greenriver.it.foodieblog.model.User;
+import edu.greenriver.it.foodieblog.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController
+{
+    private UserService service;
+
+    public AuthenticationController(UserService service)
+    {
+        this.service = service;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model)
+    {
+        model.addAttribute("user", new User());
+        return "/general/register";
+    }
+}
---
+++ src/main/java/edu/greenriver/it/foodieblog/controllers/UserController.java
@@ -0,0 +1,12 @@
+package edu.greenriver.it.foodieblog.controllers;
+
+import edu.greenriver.it.foodieblog.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController
+{
+
+}
