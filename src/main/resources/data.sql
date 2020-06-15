707beedc50f7ca99bbd8a27735399e9164af7c5b
alvinnava13 <anava8@mail.greenriver.edu>
2020-06-14 23:18:05 -0700

    Add a few test recipes into in-memory database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,8 @@ INSERT INTO user (first_name, last_name, username, password) VALUES
 ('Jaden', 'Smith', 'karatekid', '$2a$10$UUzt5vuagwwRgKBdjguksugN4bGPM7mjKZP1gtrlegaAkxXcrVfJu'),
 ('Kevin', 'Durant', 'easymoneysniper', '$2a$10$UUzt5vuagwwRgKBdjguksugN4bGPM7mjKZP1gtrlegaAkxXcrVfJu'),
 ('Paul', 'Walker', 'legend', '$2a$10$UUzt5vuagwwRgKBdjguksugN4bGPM7mjKZP1gtrlegaAkxXcrVfJu');
+
+INSERT INTO recipe (category, recipe_title, description, time_to_cook, posted) VALUES
+('Lunch', 'Salad', 'Yummy beat salad', '5 min', 'June 14, 2020'),
+('Breakfast', 'Oatmeal', 'Granola', '2 min', 'June 15, 2020'),
+('Dinner', 'Lasagna', 'Tasty and cheesy', '45 min', 'June 20, 2020');
